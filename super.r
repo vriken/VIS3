b7ca6f61adb68d88f868b3976f730e4088aa3807
vriken <vrikenz@gmail.com>
2020-11-02 10:06:57 +0100

    finished code for assignment 1b

--- super.r
+++ super.r
@@ -1,8 +1,11 @@
 library("ggplot2")
 library("GGally")
+#här laddar jag in lite libraries
 data <- read.csv("https://raw.githubusercontent.com/vriken/VIS3/main/Data/husdjur.csv")
-#här laddar jag in data från github, och använder mig av check.names = FALSE.
-#I fallet att jag inte använde check.names... Så fick jag en massa X fram
-ggplot2::theme_set(ggplot2::theme_bw())
+#här laddar jag in data från github, och tilldelar det till data-variabeln
+ggplot2::theme_set(ggplot2::theme_bw()) # här sätter jag theme globalt i alla ggplot2 funktioner till
+#theme_bw
 
-ggpairs(data)
+ggpairs(data,
+        upper = "blank",
+        diag = "blank") # här använder jag mig av ggpairs funktionen på mitt dataset

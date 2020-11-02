c32476123cc7a51b9cb909c1ec9e67048acf1249
vriken <vrikenz@gmail.com>
2020-11-02 10:04:45 +0100

    finished code for assignment 1b

--- super.r
+++ super.r
@@ -1,6 +1,8 @@
 library("ggplot2")
 library("GGally")
-data <- read.csv("https://raw.githubusercontent.com/vriken/VIS3/main/Data/husdjur.csv", check.names = FALSE)
+data <- read.csv("https://raw.githubusercontent.com/vriken/VIS3/main/Data/husdjur.csv")
+#här laddar jag in data från github, och använder mig av check.names = FALSE.
+#I fallet att jag inte använde check.names... Så fick jag en massa X fram
 ggplot2::theme_set(ggplot2::theme_bw())
 
 ggpairs(data)

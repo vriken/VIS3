281e53a90c72c4fc61547f640b5f75433d67f2e7
vriken <vrikenz@gmail.com>
2020-11-02 15:14:56 +0100

    changed the names for assignments, and added data for assignment #2

---
+++ VIS3UppgiftTva.r
@@ -0,0 +1,27 @@
+library("ggplot2")
+library("dplyr")
+data <- read.csv("C:\\Users\\vrike\\Documents\\R\\VIS3\\Data\\telefon.csv", check.names = FALSE)
+ggplot2::theme_set(ggplot2::theme_bw())
+
+
+data <- pivot_longer(data, cols = 2:7)
+ggplot(data = data,
+       aes(x = År,
+           y = value,
+           group = name,
+           color = name)) +
+  
+  labs(caption = "Källa: SCB",
+       title = "Utveckling av olika åldersgruppers eftergymnasiala utbildning",
+       color = "Ålders-\ngrupper") +
+  
+  ylab("Procent") +
+  
+  theme(axis.title.y = element_text(angle = 0,
+                                    vjust = 0.6,
+                                    size = 11),
+        legend.text = element_text(size = 11),
+        legend.key.size = unit(2, "line"),
+        plot.caption = element_text(size = 8, hjust = 0, face = "italic", color = "navy")) +
+  geom_line() 
+  

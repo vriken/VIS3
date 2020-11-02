ce80eb6567225fc92b726915e31ee2f03e4171c6
vriken <vrikenz@gmail.com>
2020-11-02 10:00:35 +0100

    finished code for assignment 1b

--- super.r
+++ super.r
@@ -1,2 +1,6 @@
 library("ggplot2")
-setwd("C:\\Users\\vrike\\Desktop")
+library("GGally")
+data <- read.csv("https://raw.githubusercontent.com/vriken/VIS3/main/Data/husdjur.csv", check.names = FALSE)
+ggplot2::theme_set(ggplot2::theme_bw())
+
+ggpairs(data)
